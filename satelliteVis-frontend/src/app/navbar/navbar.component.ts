611c4b0b1df833f7d0f6d0aaa408bc71f521bd02
i1nac <egeguler2003@gmail.com>
2025-05-03 21:51:16 +0300

    added navbar

--- satelliteVis-frontend/src/app/navbar/navbar.component.ts
+++ satelliteVis-frontend/src/app/navbar/navbar.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  standalone: true,
+  imports: [MatButtonModule, MatToolbarModule, RouterModule, MatIconModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
 
+    constructor(private matIconRegistry: MatIconRegistry, private domSanitizer: DomSanitizer){
+      this.matIconRegistry.addSvgIcon(
+        "github",
+        this.domSanitizer.bypassSecurityTrustResourceUrl("assets/github.svg")
+      )
+    }
 }
